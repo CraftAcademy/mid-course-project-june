67b9c0745eeb5f2226437523db52d0eb2cd86cd4
MikaelFeher <mikael.feher@gmail.com>
2016-08-02 16:00:56 +0200

    App scaffold (#2)
    * scaffolding rails and setup gemfile
    
    * setup rspec test environment
    
    * setup cucumber test environment
    
    * update .gitignore and adding travis yaml file
    
    * setup heroku and travis deployment
    
    * adding coveralls and rspec/cucumber merge

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MidCourseProjectJune
+  class Application < Rails::Application
+    # Disable generation of helpers, javascripts, css, and view, helper, routing and controller specs
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
+  end
+end
