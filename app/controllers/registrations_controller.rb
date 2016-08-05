f18a91794d2455883276eb22a7d70e20472b0b97
MikaelFeher <mikael.feher@gmail.com>
2016-08-05 09:46:14 +0200

    remove unnecessary sign up method in the registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -26,8 +26,4 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     new_orphanage_path
   end
-
-  def sign_up_params
-    params.require(:user).permit(:type, :email, :password, :password_confirmation)
-  end
 end
